1ae2b047eea84fdff0a37baddcd9e1a1204b1f2e
zineb sairouj <zineb.halit@gmail.com>
2023-06-16 13:12:40 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nmbr;
+
+	for (nmbr = 0; nmbr <= 9; nmbr++)
+	{
+		putchar((nmbr % 10) + '0');
+		if (nmbr == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
