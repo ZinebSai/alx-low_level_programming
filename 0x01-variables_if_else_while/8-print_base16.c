61616c9e68ae13ab21afd5b6d832db5b30693b1d
zineb sairouj <zineb.halit@gmail.com>
2023-06-16 12:50:34 -0700

    Hexadecimal , changes in Numberz file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints single digit numbers of base 10 starting from 0.
+ * main - Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int nmbr;
+	char alph;
 
 	for (nmbr = 0; nmbr < 10; nmbr++)
 		putchar((nmbr % 10) + '0');
+
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
 
 	putchar('\n');
 
