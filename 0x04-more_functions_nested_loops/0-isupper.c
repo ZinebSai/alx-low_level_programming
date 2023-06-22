55e48942c2e1a8933734b0a59d58c953dd7d7b84
zineb sairouj <zineb.halit@gmail.com>
2023-06-22 07:25:01 -0700

    isupper task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase charachter.
  * @c : is the character.
- * Return : 1 if it is a uppercase character, 0 if it is otherwise.
+ * * Return: 1 if uppercase else 0 .
  */
 
 int _isupper(int c)
