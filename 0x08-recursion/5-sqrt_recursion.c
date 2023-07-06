e52a6380051ba318161bfa1f1a693b07d5d3839c
zineb sairouj <zineb.halit@gmail.com>
2023-07-06 16:29:11 -0700

    recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * sqrt2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
- *     *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int sqrt2(int a, int b)
+ *  * my_sqrt_recursion - This is my my_sqrt_recursion
+ *   *                     function about the square root
+ *    * @a: The entry is equal to n, of the before function
+ *     * @b: This is the sum
+ *      *
+ *       * Return: This is the result
+ *        */
+int my_sqrt_recursion(int a, int b)
 {
-	if (b * b == a)
+	if (a == (b * b))
 		return (b);
-	else if (b * b > a)
+	else if ((b * b) >= a)
 		return (-1);
-	return (sqrt2(a, b + 1));
+	else
+		return (my_sqrt_recursion(a, b + 1));
 }
+
 /**
- *  * _sqrt_recursion - returns the natural square root of n
- *   * @n: Number Integer
+ *  * _sqrt_recursion - This is my first function
+ *   * @n: This is my value
  *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ *     * Return: This is my result of the function my_sqrt_recursion
+ *      */
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+	if (n <= 0)
+		return (-1);
+	return (my_sqrt_recursion(n, 0));
 }
